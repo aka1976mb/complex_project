a8d3c7d4236968d2657ae4b92c2dcc128d5ec3cf
Your Name <your.email@example.com>
2025-12-02 02:51:33 +0000

    Initial

---
+++ functions.h
@@ -0,0 +1,5 @@
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
+int add(int a, int b);
+int multiply(int a, int b);
+#endif
---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include "functions.h"
+#include <iostream>
+int main() {
+    std::cout << "Sum: " << add(5, 3) << std::endl;
+    std::cout << "Product: " << multiply(5, 3) << std::endl;
+    return 0;
+}
